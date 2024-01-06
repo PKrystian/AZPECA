5a89fa485a8563eab286ae9665f76d50dd9c2bf7
PKrystian <krystian.pinczak2001@gmail.com>
2024-01-06 19:07:26 +0100

    part 1, init

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using User.Function;
+using User.Function.Database;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
